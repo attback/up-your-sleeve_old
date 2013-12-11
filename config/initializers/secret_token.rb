317d4e0c1d268e7109640085cbbf921b854c58e2
Josefin Attback <josefin.attback@xdin.com>
2013-12-11 12:10:05 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UpYourSleeve::Application.config.secret_token = '32bf01fe636d0a52b6228e41dc3c21a013dd2f051c9a66f77693cbc8ac8006a6ce69557e36474cc93bfa0d9d957c269d73b3e609f775d8b854fd81ebef53e57a'
